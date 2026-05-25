fd49da7de529fa683653dd07eb733c37373691fa
kanalsop <207140925+kanalsop@users.noreply.github.com>
2026-05-25 09:09:52 +0900

    feat(nvm): Add nvm initialization to macos.zsh

--- zsh/.config/zsh/macos.zsh
+++ zsh/.config/zsh/macos.zsh
@@ -6,3 +6,8 @@
 
 # Antigravity
 [[ -d "$HOME/.antigravity/antigravity/bin" ]] && export PATH="$HOME/.antigravity/antigravity/bin:$PATH"
+
+# nvm
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
